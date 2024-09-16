454ed4e8ba8cef08a39a67389ce285f76e9f76b2
Erkki Pulliainen <erkki.pulliainen@nitor.com>
2024-09-16 11:45:42 +0300

    Configurable SLACK_URL for webhook

--- runFromGithub.sh
+++ runFromGithub.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
+# Copy webhook URL here
+## SLACK_URL=
 curl -fsSL https://raw.githubusercontent.com/NitorCreations/IntuneMDM/main/App-Auto-Patch-via-Dialog-Nitor.zsh | sudo /bin/zsh -s > /tmp/AppAutoPatch.log >& /tmp/AppAutoPatch.err
 echo "Ran AppAutoPatch at $(date)" > /tmp/AppAutoPatch.time 
